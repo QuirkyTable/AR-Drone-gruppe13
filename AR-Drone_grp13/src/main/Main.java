1c418374f0f08d60be48b83d53615cc1ab0527fc
Christian <Christian@QuirkysLaptop>
2016-02-10 14:09:57 +0100

    First commit

---
+++ AR-Drone_grp13/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import de.yadrone.base.ARDrone;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ARDrone drone = new ARDrone();
+		System.out.println(drone.getSpeed());
+	}
+
+}
